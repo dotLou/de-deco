5b1234777a8a287bf2af2d6dd65f9de41042149d
Louis Cloutier <me@dotlou.com>
2013-04-20 12:37:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeDeco::Application.config.secret_token = '78f12114e823b90b999eef037a9563335396b9fad9cc56469c5032b9da2ddcce087d36675ca0265bfe858d15de6dcedf2e8f2bfb816cd8017fd0fc720e07fedb'
